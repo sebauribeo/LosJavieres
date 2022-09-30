0f56ec44895600e001a3b62f4c030a85457a9474
sebauribeo <chefsebauribe@gmail.com>
2022-09-30 13:13:54 -0300

    add new models and formModels

--- Botilleria/forms.py
+++ Botilleria/forms.py
@@ -1,45 +1,46 @@
 from django import forms
-from .models import Product
+from .models import Product, User
+
 
 class ProductForm(forms.ModelForm):
 
     name = forms.CharField(widget=forms.TextInput(
-        attrs = {
+        attrs={
             'class': 'form-control mt-2',
             'type': 'text',
             'placeholder': 'Ingresa el nombre del producto'
         }
     ))
     image = forms.CharField(widget=forms.TextInput(
-        attrs = {
+        attrs={
             'class': 'form-control mt-2',
             'type': 'text',
             'placeholder': 'Ingresa la URL de la imagen'
         }
     ))
     price = forms.CharField(widget=forms.TextInput(
-        attrs = {
+        attrs={
             'class': 'form-control mt-2',
             'type': 'number',
             'placeholder': 'Ingresa el precio del producto'
         }
     ))
     stock = forms.CharField(widget=forms.TextInput(
-        attrs = {
+        attrs={
             'class': 'form-control mt-2',
             'type': 'text',
             'placeholder': 'Ingresa el stock'
         }
     ))
     details = forms.CharField(widget=forms.TextInput(
-        attrs = {
+        attrs={
             'class': 'form-control mt-2',
             'type': 'text',
             'placeholder': 'Ingresa la descipción del producto'
         }
     ))
     brand = forms.CharField(widget=forms.TextInput(
-        attrs = {
+        attrs={
             'class': 'form-control mt-2',
             'type': 'text',
             'placeholder': 'Ingresa la marca del producto'
@@ -49,4 +50,38 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ('name', 'image', 'price', 'stock', 'details', 'brand')
-     
+
+
+class UserForm(forms.ModelForm):
+
+    name = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control mt-2',
+            'type': 'text',
+            'placeholder': 'Ingresa tu nombre'
+        }
+    ))
+    lastName = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control mt-2',
+            'type': 'text',
+            'placeholder': 'Ingresa tu apellido'
+        }
+    ))
+    email = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control mt-2',
+            'type': 'email',
+            'placeholder': 'Ingresa el email'
+        }
+    ))
+    password = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control mt-2',
+            'type': 'password',
+            'placeholder': 'Ingresa la contraseña'
+        }
+    ))
+    class Meta:
+        model = User
+        fields = ('name', 'lastName', 'email', 'password')
--- Botilleria/models.py
+++ Botilleria/models.py
@@ -11,3 +11,29 @@ class Product(models.Model):
         brand = models.CharField(max_length = 50)
     except Exception as e:
         print('Modelo no creado: ', e)
+
+class User(models.Model):
+    try:
+        name = models.CharField(min_lenght = 50)
+        lastName = models.CharField(min_lenght = 50)
+        email = models.CharField(min_lenght = 100, unique = True)
+        password = models.CharField(min_lenght = 100)
+    except Exception as e:
+        print('Modelo no creado: ', e)
+
+# class promotionalProducts(models.Model):
+#     try:
+#         image = models.CharField(min_lenght = 500)
+#         description = models.CharField(min_lenght = 500)
+#         price = models.IntegerField()
+#     except Exception as e:
+#         print('Modelo no creado: ', e)
+
+# class ShoppingCart(models.Model):
+#     try:
+#         product = models.CharField(min_lenght = 100)
+#         price = models.IntegerField()
+#         quantity = models.IntegerField()
+#         user = models.CharField(min_lenght = 100)
+#     except Exception as e:
+#         print('Modelo no creado: ', e)
